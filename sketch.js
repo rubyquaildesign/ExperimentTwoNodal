fc6f88ace08efb16aaec4aa48e645ce6f5a44ab3
Rupertofly <rupertqmoore@me.com>
2018-04-03 14:50:01 +1000

    ♻️ Cleaning Example Code

--- sketch.js
+++ sketch.js
@@ -1,70 +1,11 @@
 /* eslint no-undef: 0 */
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "setup|draw" }] */
-let someSprites = [];
-function setup () {
-  createCanvas(500, 400);
-
-  for (let index = 0; index < 35; index++) {
-    someSprites[index] = createSprite(
-      random(width - 100) + 50,
-      random(height - 100) + 50,
-      30,
-      30
-    );
-    someSprites[index].mouseActive = true;
-    someSprites[index].maxSpeed = 0.6;
-    someSprites[index].velocity = createVector(random(-1, 1), random(-1, 1));
-    someSprites[index].wanderAngle = 0;
-  }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 }
-function draw () {
+function draw() {
   background(55);
-  for (let i = 0; i < someSprites.length; i++) {
-    const spr = someSprites[i];
-    if (spr.mouseIsPressed) {
-      spr.position.x = mouseX;
-      spr.position.y = mouseY;
-      spr.maxSpeed = 0;
-      spr.maxSpeed = 100;
-      spr.width = 50;
-      spr.height = 50;
-    } else {
-      spr.width = 30;
-      spr.height = 30;
-
-      wander(spr);
-    }
-  }
-  drawSprites();
-}
-function wander (_sprite) {
-  _sprite.maxSpeed = 1;
-
-  let vW = _sprite.velocity.copy();
-  vW.normalize();
-  vW.mult(30);
-  let vW2 = vW.copy();
-  vW2.add(_sprite.position);
-  // ellipse(vW2.x, vW2.y, 10, 10);
-  let displacement = createVector(0, -1);
-  displacement.mult(3);
-  displacement.rotate(_sprite.wanderAngle);
-  _sprite.wanderAngle += random() * 1.5 - 1.5 * 0.5;
-  wanderForce = vW.add(displacement);
-  // line(vW2.x, vW2.y, vW2.x + displacement.x * 10, vW2.y + displacement.y * 10);
-  _sprite.velocity.add(wanderForce);
-  _sprite.maxSpeed = 100;
-  _sprite.restitution = 1.5;
-  _sprite.bounce(allSprites);
-  _sprite.maxSpeed = 1;
-  if (_sprite.position.x > width - 50) {
-    _sprite.velocity.x -= 0.8;
-  } else if (_sprite.position.x < 50) {
-    _sprite.velocity.x += 0.8;
-  } else if (_sprite.position.y > height - 50) {
-    _sprite.velocity.y -= 0.8;
-  } else if (_sprite.position.y < 50) {
-    _sprite.velocity.y -= 0.8;
-  } else {
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, 30, 30)
   }
 }
