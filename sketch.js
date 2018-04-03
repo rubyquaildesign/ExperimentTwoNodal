0e35e2a531aca8a981928373b445e69b81dd4f62
Rupertofly <rupertqmoore@me.com>
2018-04-03 18:04:08 +1000

    ➕ Added Pallete

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 /* eslint no-undef: 0 */
-/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "setup|draw" }] */
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "setup|draw|preload" }] */
+
+function preload () {}
+
 function setup () {
   createCanvas(windowWidth, windowHeight);
 }
 function draw () {
-  background(55);
+  background(Pallete.neutrals[1].hex);
   if (mouseIsPressed) {
     ellipse(mouseX, mouseY, 30, 30);
   }
