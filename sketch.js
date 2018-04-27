5d51048e763f5c0f86223805b408cf05f535ea23
Rupertofly <rupertqmoore@me.com>
2018-04-27 14:49:20 +1000

    clean up a few things

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 // @ts-check
 /* eslint no-undef: 0 */
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "setup|draw|preload|recordFrame|recordSetup|p5Canvas0" }] */
+// region SEC: Globals
 
-function preload() {}
 let p5Canvas0;
+
+// endregion
+function preload() {}
 
 function setup() {
   p5Canvas0 = createCanvas(300, 500);
 }
+
 function draw() {}
